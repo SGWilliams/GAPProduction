c0f5559fa1760430f3324aa84dbcaa2339e551e6
Nathan Tarr <nmtarr@ncsu.edu>
2016-07-11 15:28:10 -0400

    test

---
+++ gappack/__init__.py
@@ -0,0 +1,3 @@
+import gapdb, gapmodeling, dictionaries, conversions, beta, arcmap, gaprasters, match_and_filter
+__all__ = ['gapdb', 'gapmodeling', 'dictionaries', 'conversions', 'beta',
+            'arcmap' 'gaprasters', 'match_and_filter']
