cbad81988cd8922d566df47240252f137d20f07c
Nathan M. Tarr <nmtarr@BaSIC.local>
2016-07-13 12:39:37 -0400

    Deleted old unused files

--- gappack/__init__.py
+++
@@ -1,3 +0,0 @@
-import gapdb, gapmodeling, dictionaries, conversions, beta, arcmap, gaprasters, match_and_filter
-__all__ = ['gapdb', 'gapmodeling', 'dictionaries', 'conversions', 'beta',
-            'arcmap' 'gaprasters', 'match_and_filter']
