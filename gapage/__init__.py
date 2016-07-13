63f202f397a89ffa0ae740b9ec107e6c9bfd9baa
Nathan M. Tarr <nmtarr@BaSIC.local>
2016-07-13 15:34:55 -0400

    Added the states module from gappack

--- gapage/__init__.py
+++ gapage/__init__.py
@@ -1,6 +1,6 @@
 import gapemail, docs, gapmath, gapdb, gapmodeling, dictionaries, conversions, \
-        beta, gaprasters, match_and_filter
+        beta, gaprasters, match_and_filter, states
 
 __all__ = ['gapemail', 'docs', 'gapmath', 'gapdb', 'gapmodeling', 
            'dictionaries', 'conversions', 'beta', 'gaprasters', 
-           'match_and_filter']
+           'match_and_filter', 'states']
