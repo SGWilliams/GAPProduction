a6f426a7fcdccb0626894eb8437f2c242c6c1b42
Nathan M. Tarr <nmtarr@BaSIC.local>
2016-07-13 16:10:13 -0400

    Added the tables and featureclasses modules from gappack

--- gapage/__init__.py
+++ gapage/__init__.py
@@ -1,6 +1,6 @@
 import gapemail, docs, gapmath, gapdb, gapmodeling, dictionaries, conversions, \
-        beta, gaprasters, match_and_filter, states
+        beta, gaprasters, match_and_filter, states, tables, featureclasses
 
 __all__ = ['gapemail', 'docs', 'gapmath', 'gapdb', 'gapmodeling', 
            'dictionaries', 'conversions', 'beta', 'gaprasters', 
-           'match_and_filter', 'states']
+           'match_and_filter', 'states', 'tables', 'featureclasses']
