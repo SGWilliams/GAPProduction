1a315a11908fb8a4a63831eb08fcd92dd3c6341d
Nathan M. Tarr <nmtarr@BaSIC.local>
2016-07-15 09:43:37 -0400

    Added gapageconfig to __init__ and renamed config template

--- gapage/__init__.py
+++ gapage/__init__.py
@@ -1,6 +1,6 @@
 import gapemail, docs, gapmath, gapdb, gapmodeling, dictionaries, conversions, gaprange, \
         beta, gaprasters, match_and_filter, states, tables, featureclasses, zipping, gaprichness, \
-        spatial, gapmetadata, webservices
+        spatial, gapmetadata, webservices, gapageconfig
 
 __all__ = ['gapemail', 'docs', 'gapmath', 'gapdb', 'gapmodeling', 'dictionaries', 'conversions', 
            'gaprange', 'beta', 'gaprasters', 'match_and_filter', 'states', 'tables', 
