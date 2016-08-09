9350f76d921e9db4bf1f4293ae2eacde69c2e874
Nathan M. Tarr <nmtarr@BaSIC.local>
2016-08-09 12:10:12 -0400

    I made the config file template more readable

--- gapageconfig_template.py
+++ gapageconfig_template.py
@@ -1,52 +1,56 @@
 #This is the config file for gapage.  Store it somewhere other than with the gapage code and data.
 
+############################     STUFF YOU HAVE TO CHANGE   ########################################
+
 #Directory with spatial and aspatial datasets needed for gappack
 data_directory = "xxxxxx/GAPage/data"
 
 #Directory where some temp files should be stored (scratch workspace)
 temp_directory = "xxxxxxxx"
 
-#Pickle file with dictionary of wildlife centric classification
-wildclass = data_directory + "/WildlifeMUClassification.pkl"
+#Path to modeling data layers
+GAP_data = "xxx/xxxx/xxx"
+
+#Path to directory containing completed species model outputs GEOtiffs for analyses
+output_location = "xxx/xxxx/xxx"
+
+#Directory to store output from richness analyses
+richness_directory = "xxx/xxxx/xxx"
 
 # Username and password for the GAP databases
 server = ""
 uid = raw_input("Username: ")
 password = raw_input("Password: ")
 
-#Path to modeling data layers
-GAP_data = "xxx/xxxx/xxx"
+#Gapemail passwords
+emailAddress = ""
+emailPwd = ""
+emailAccountServer = ""
+emailDefaultToAddress = ""
 
-#Path to the snap raster
-snap_raster = data_directory + "/snapgrid"
+
+####################    STUFF YOU DON'T HAVE TO CHANGE  ############################################
 
 #The path to the CONUS HUCs shapefile
 hucs = GAP_data + "/Hucs.shp"
 
-#Path to table with extents of each huc in Albers
-HUC_Extents = data_directory + "/HUC_Extents.txt"
+#Path to directory containing Land Cover rasters used in modeling.
+land_cover = GAP_data + "/LandCover"
 
 #Path to directory containing regional boundary shapefiles
 region_shapefiles = GAP_data + "/regions"
 
+#Pickle file with dictionary of wildlife centric classification
+wildclass = data_directory + "/WildlifeMUClassification.pkl"
+
+#Path to the snap raster
+snap_raster = data_directory + "/snapgrid"
+
+#Path to table with extents of each huc in Albers
+HUC_Extents = data_directory + "/HUC_Extents.txt"
+
 #Path to shapefile of all regions
 regions_shapefile = data_directory + "modeling_regions.shp"
-
-#Path to directory containing Land Cover rasters used in modeling.
-land_cover = GAP_data + "/LandCover"
-
-#Path to directory containing completed species model outputs GEOtiffs for analyses
-output_location = "xxx/xxxx/xxx"
-
-#Added 7/13/2016
-#Gapemail passwords
-emailAddress = ""
-emailPwd = ""
-emailAccountServer = ''
-emailDefaultToAddress = ""
-
-#Directory to store output from richness analyses
-richness_directory = "xxx/xxxx/xxx"
 
 #Directory with metadata templates
 meta_templates_dir = data_directory + "/metadata_templates"
