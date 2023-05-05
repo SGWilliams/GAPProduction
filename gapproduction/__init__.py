3111df83bd932e55190500a385d7410c1cd885da
Nathan Tarr <nmtarr@ncsu.edu>
2023-05-05 11:04:10 -0400

    Deleted content that isn't immediately of value.  Updated the rest of code to Python 3.x.

--- gapproduction/__init__.py
+++ gapproduction/__init__.py
@@ -1,8 +1,5 @@
-import gapemail, docs, gapmath, gapdb, gapmodeling, dictionaries, conversions, gaprange, \
-        beta, gaprasters, match_and_filter, states, tables, featureclasses, zipping, gaprichness, \
-        spatial, gapmetadata, webservices, gapconfig, landcover, wildclass, sciencebase, taxonomy
+from gapproduction import beta, dictionaries, documents, \
+    database, strings
 
-__all__ = ['gapemail', 'docs', 'gapmath', 'gapdb', 'gapmodeling', 'dictionaries', 'conversions', 
-           'gaprange', 'beta', 'gaprasters', 'match_and_filter', 'states', 'tables', 
-           'featureclasses', 'zipping', 'gaprichness', 'spatial', 'gapmetadata', 'webservices',
-           'wildclass', 'sciencebase', 'taxonomy', 'landcover']
+__all__ = ['beta', 'conversions', 'dictionaries', 'documents', 'database', 
+           'strings']
