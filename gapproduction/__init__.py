559d065ca6b90dd96644fe6526978ef6720dfb98
Nathan Tarr <nmtarr@ncsu.edu>
2023-05-05 11:32:56 -0400

    Readme formatting.

--- gapproduction/__init__.py
+++
@@ -1,5 +0,0 @@
-from gapproduction import beta, dictionaries, documents, \
-    database, strings
-
-__all__ = ['beta', 'conversions', 'dictionaries', 'documents', 'database', 
-           'strings']
